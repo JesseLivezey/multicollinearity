41923cc3462d4e930c806fef434f2807ae90a04d
Jesse Livezey <jesse.livezey@gmail.com>
2017-08-10 17:42:19 -0700

    moving functions to py file

---
+++ analysis.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def sample_data(X_cov, M, noise_std, n_data, rng):
+    X = rng.multivariate_normal(np.zeros_like(M), X_cov, n_data)
+    X /= X.std(axis=0, keepdims=True)
+    Y = X.dot(M) + rng.randn(n_data) * noise_std
+    return X, Y
+
+def fit_linear(X, Y):
+    return LinR(fit_intercept=False).fit(X, Y).coef_
+
+def fit_lasso(X, Y, alpha):
+    return LasR(alpha=.1, fit_intercept=False).fit(X, Y).coef_
+
+def fit_lasso_linear(X, Y, alpha):
+    las = fit_lasso(X, Y, alpha)
+    if np.count_nonzero(las) < X.shape[1]:
+        result = np.zeros_like(las)
+        nz = np.nonzero(las)[0]
+        Xp = X[:, nz]
+        lin = fit_linear(Xp, Y)
+        result[nz] = lin
+        return result
+    else:
+        return las
+
+def lin_cost(X, Y, M):
+    n = M.shape[0]
+    cost = np.zeros(n)
+    for ii, Mi in enumerate(M):
+        cost[ii] = np.mean((Y - X.dot(Mi))**2)
+    return cost
+
+def abs_cost(M, alpha):
+    n = M.shape[0]
+    cost = np.zeros(n)
+    for ii, Mi in enumerate(M):
+        cost[ii] = alpha * np.sum(abs(Mi))
+    return cost
+
+def las_cost(X, Y, M, alpha):
+    return lin_cost(X, Y, M) + abs_cost(M, alpha)
